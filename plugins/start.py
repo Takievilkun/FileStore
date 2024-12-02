ff80c67fe85a2e731bacc3cbcf8c2544d60895de
Takievil <138558442+Takievilkun@users.noreply.github.com>
2024-12-03 00:29:35 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -1,5 +1,5 @@
 import os
-import asyncio, 
+import asyncio
 from pyrogram import Client, filters, __version__
 from pyrogram.enums import ParseMode
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
